a69d93b13ae31afcc0444c4080e7fcce71543034
Feng Stone <redpark@qq.com>
2019-11-19 15:28:52 +0800

    log-printing problem

--- Framework/MetalTensor/metal_tensor_log.c
+++ Framework/MetalTensor/metal_tensor_log.c
@@ -21,8 +21,7 @@ void db_print(int level, const char *fmt, ...)
     {
         va_list args;
         va_start(args, fmt);
-        printf(fmt, args);
+        vprintf(fmt, args);
         va_end(args);
     }
 }
-
